3001cad16f0f3f43d36ab97afedabd5a5160d906
Kimberlee.Mendoza@Gmail.com <Kimberlee.Mendoza@Gmail.com>
2020-04-16 01:44:17 -0400

    TSG

---
+++ HelloWorld/src/main/java/com/kimberlee/helloworld/Hello.java
@@ -0,0 +1,14 @@
+
+
+package com.kimberlee.helloworld;
+
+/**
+ *
+ * @author Max Mendoza
+ */
+public class Hello {
+    public static void main (String [] args ) {
+        System.out.println("Hello, World");
+        System.out.println("My name is Harry!");
+    }
+}
