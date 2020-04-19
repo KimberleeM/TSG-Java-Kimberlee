35f9c12a7a4f456ff2405b184bf83a888997c025
Kimberlee.Mendoza@Gmail.com <Kimberlee.Mendoza@Gmail.com>
2020-04-19 17:36:15 -0400

    code along

--- HelloWorld/src/main/java/com/kimberlee/CodeAlong/WindowMaster.java
+++ HelloWorld/src/main/java/com/kimberlee/CodeAlong/WindowMaster.java
@@ -16,12 +16,14 @@ public class WindowMaster {
         float width;
         float cog;
         float cot;
+        float now;
 
         //declare String variables for height and width input
         String stringHeight;
         String stringWidth;
         String stringcog;
         String stringcot;
+        String stringnow;
 
         //declare other variables 
         float areaOfWindow;
@@ -40,34 +42,33 @@ public class WindowMaster {
         stringcog = myScanner.nextLine();
         System.out.println("Cost of trim: ");
         stringcot = myScanner.nextLine();
-
-        
+        System.out.println("Number of windows: ");
+        stringnow = myScanner.nextLine();
 
         //convert String values of height and width to float values 
         height = Float.parseFloat(stringHeight);
         width = Float.parseFloat(stringWidth);
         cog = Float.parseFloat(stringcog);
         cot = Float.parseFloat(stringcot);
+        now = Float.parseFloat(stringnow);
 
         //calculate the area of window 
         areaOfWindow = height * width;
 
         //calculate the perimeter of the window
         perimeterOfWindow = 2 * (height + width);
-        
+
         //calculate the total cost - use a hard-coded value for material cost 
-        cost = ((cog * areaOfWindow) + (cot * perimeterOfWindow));
-        
+        cost = (((cog * areaOfWindow) + (cot * perimeterOfWindow)) * (now));
+
         //display the results to the user 
         System.out.println("Window height = " + stringHeight);
         System.out.println("Window width = " + stringWidth);
         System.out.println("Window area = " + areaOfWindow);
-        System.out.println("Window perimeter = " + perimeterOfWindow );
+        System.out.println("Window perimeter = " + perimeterOfWindow);
         System.out.println("Total Cost = " + cost);
 
-        
         //testing values: height = 2. widht = 3, cog = 3.50,
         //at the end, test 2.25 in the trim cost and make sure total cost is 43.50
     }
 }
- 
