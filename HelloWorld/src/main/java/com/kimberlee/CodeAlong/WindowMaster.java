0f68dc86c1a8835eb71b16c894019de2c12cbbfe
Kimberlee.Mendoza@Gmail.com <Kimberlee.Mendoza@Gmail.com>
2020-04-19 16:28:54 -0400

    code along

--- HelloWorld/src/main/java/com/kimberlee/CodeAlong/WindowMaster.java
+++ HelloWorld/src/main/java/com/kimberlee/CodeAlong/WindowMaster.java
@@ -30,19 +30,28 @@ public class WindowMaster {
         //get input from user 
         System.out.println("Please enter window height:");
         stringHeight = myScanner.nextLine();
-
         System.out.println("Please enter window width:");
         stringWidth = myScanner.nextLine();
-        
+
         //convert String values of height and width to float values 
         height = Float.parseFloat(stringHeight);
         width = Float.parseFloat(stringWidth);
-        
+
         //calculate the area of window 
         areaOfWindow = height * width;
-        
+
         //calculate the perimeter of the window
         perimeterOfWindow = 2 * (height + width);
         
+        //calculate the total cost - use a hard-coded value for material cost 
+        cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
+        
+        //display the results to the user 
+        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window area = " + areaOfWindow);
+        System.out.println("Window perimeter = " + perimeterOfWindow );
+        System.out.println("Total Cost = " + cost);
+
     }
 }
