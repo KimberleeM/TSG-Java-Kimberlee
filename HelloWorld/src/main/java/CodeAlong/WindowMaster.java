794adeda4eea46e01b91fb76638011a42fda405d
Kimberlee.Mendoza@Gmail.com <Kimberlee.Mendoza@Gmail.com>
2020-04-19 03:34:54 -0400

    code along

---
+++ HelloWorld/src/main/java/CodeAlong/WindowMaster.java
@@ -0,0 +1,48 @@
+package CodeAlong;
+
+/**
+ *
+ * @author Max
+ */
+import java.util.Scanner;
+
+public class WindowMaster {
+
+    public static void main(String[] args) {
+
+        //alt + shift + f
+        //declare variables for height and width 
+        float height;
+        float width;
+
+        //declare String variables for height and width input
+        String stringHeight;
+        String stringWidth;
+
+        //declare other variables 
+        float areaOfWindow;
+        float cost;
+        float perimeterOfWindow;
+
+        //declare and initialize the Scanner
+        Scanner myScanner = new Scanner(System.in);
+
+        //get input from user 
+        System.out.println("Please enter window height:");
+        stringHeight = myScanner.nextLine();
+
+        System.out.println("Please enter window width:");
+        stringWidth = myScanner.nextLine();
+        
+        //convert String values of height and width to float values 
+        height = Float.parseFloat(stringHeight);
+        width = Float.parseFloat(stringWidth);
+        
+        //calculate the area of window 
+        areaOfWindow = height * width;
+        
+        //calculate the perimeter of the window
+        perimeterOfWindow = 2 * (height + width);
+        
+    }
+}
