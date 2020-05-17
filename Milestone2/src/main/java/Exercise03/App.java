f58b36b40e6c1c4d180d3173a9b81dcbdef1bd7b
Kimberlee.Mendoza@Gmail.com <Kimberlee.Mendoza@Gmail.com>
2020-05-16 20:01:00 -0400

    exercises

--- Milestone2/src/main/java/Exercise03/App.java
+++ Milestone2/src/main/java/Exercise03/App.java
@@ -4,11 +4,49 @@ package Exercise03;
  *
  * @author Max
  */
+import java.util.Scanner;
+
 public class App {
-    
+
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
         SimpleCalculator sc = new SimpleCalculator();
-        sc.run();
+
+        //while (true) {
+        System.out.println("Which operation would you like to use (+,/,-,*)? 1 for +, 2 for /, 3 for -,4 for *, 5 for exit ");//tells user what we want
+        int num = scan.nextInt(); //collect number from user
+
+        if (num == 5) {
+            System.out.println("Thank you");
+            System.exit(0);
+        }
+        
+        else{
+            
+        
+            
+        System.out.println("Enter first operand");//tells user what we want
+        float op1 = scan.nextFloat(); //collect number from user
+        System.out.println("Enter second operand ");//tells user what we want
+        float op2 = scan.nextFloat(); //collect number from user
+
+        if (num == 1) {
+            System.out.println(sc.add(op1, op2));
+        }
+
+        if (num == 2) {
+            System.out.println(sc.divide(op1, op2));
+        }
+        if (num == 3) {
+            System.out.println(sc.subtract(op1, op2));
+        }
+        if (num == 4) {
+            System.out.println(sc.multiply(op1, op2));
+        }
+        
+        }
+    
     }
+
 }
--- Milestone2/src/main/java/Exercise03/SimpleCalculator.java
+++ Milestone2/src/main/java/Exercise03/SimpleCalculator.java
@@ -5,47 +5,21 @@ package Exercise03;
  * @author Max
  */
 public class SimpleCalculator {
-    public void run(){
-    }  
-    int operands1;
-    float operands2;
- 
+
     
-    public void operands1(int operands1){
-        this.operands1 = operands1;
-        
-    }
-   
-
-    public int getOperands1() {
-        return operands1;
-    }
-
-    public void setOperands1(int operands1) {
-        this.operands1 = operands1;
-    }
-
-    public float getOperands2() {
-        return operands2;
-    }
-
-    public void setOperands2(float operands2) {
-        this.operands2 = operands2;
-    }
-    
-    public float add(){
+    public float add(float  operands1, float  operands2){
         return operands1 + operands2;
     }
     
-    public float multiply(){
+    public float multiply(float  operands1, float  operands2){
         return operands1 * operands2;
     }
     
-    public float divide(){
+    public float divide(float  operands1, float  operands2){
         return operands1 / operands2;
     }
     
-    public float subtract(){
+    public float subtract(float  operands1, float  operands2){
         return operands1 - operands2;
     }
 
